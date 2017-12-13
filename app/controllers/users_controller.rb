ae9196d3278f3725069a19713937593b1658298d
Kai Zhang <kaizen.agility@gmail.com>
2017-12-13 03:30:41 -0500

    Other User controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,35 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def new
+        @user = User.new        
+    end
+
     def create
-        
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def update
-
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
     end
 
     def destroy
+        User.find(params[:id]).delete
+        redirect_to users_path
+    end
 
+    private
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end

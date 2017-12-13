14916caf4a5a4cddb3493a25cb46a6c882fce336
Kai Zhang <kaizen.agility@gmail.com>
2017-12-13 03:07:55 -0500

    Basic users index view, nested routing, rooted to Users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :jobapplications
+  root to: 'users#index'
+  
+  resources :users do
+    resources :jobapplications
+  end
 end
